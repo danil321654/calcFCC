bc5ff8ec7a7ea134284f5412ec9cae40fa7fb2fb
danil321654 <danil321654@gmail.com>
2020-01-14 16:29:18 +0300

    created interface

--- src/index.js
+++ src/index.js
@@ -1,12 +1,56 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import Redux, {createStore} from 'redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const buttonsInfo = [
+                {id: 'AC',size:'horRectangle', color: 'red', type:'act'},
+                {id: '/',size:'square', color: 'light-gray', type:'act'},
+                {id: 'x',size:'square', color: 'light-gray', type:'act'},
+                {id: '7',size:'square', type:'act'},
+                {id: '8',size:'square', type:'num'},
+                {id: '9',size:'square', type:'num'},
+                {id: '-',size:'square', color: 'light-gray', type:'act'},
+                {id: '4',size:'square', type:'num'},
+                {id: '5',size:'square', type:'num'},
+                {id: '6',size:'square', type:'num'},
+                {id: '+',size:'square', color: 'light-gray', type:'act'},
+                {id: '1',size:'square', type:'num'},
+                {id: '2',size:'square', type:'num'},
+                {id: '3',size:'square', type:'num'},
+                {id: '=',size:'vertRectangle', color: 'blue', type:'act'},
+                {id: '0',size:'horRectangle', type:'num'},
+                {id: '.',size:'square', type:'num'}
+              ];
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class CalcButton extends React.Component{
+  render(){
+    return(
+      <button id={this.props.props.id} className={"button " + this.props.props.size +" "+ this.props.props.color}> {this.props.props.id} </button>
+    )
+  }
+}
+class Calc extends React.Component{
+  render(){
+    let buttons = buttonsInfo.map((button)=> <CalcButton props={button}/>);
+    return(
+      <div id="calc" >
+
+        <div id="displays">
+              <div id="allDisp">0</div>
+
+              <div id="currentDisp">0</div>
+          </div>
+      <div id="numPad">
+        {buttons}
+        </div>
+      </div>
+    )
+  }
+}
+
+
+
+
+
+ReactDOM.render(<Calc />, document.getElementById('root'));
